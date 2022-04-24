2d013a7b176aa6f82ec3c68182495bb529f79aa4
amisha2616 <74150077+amisha2616@users.noreply.github.com>
2022-04-24 16:21:26 +0530

    chore:grid update

--- registration.js
+++ registration.js
@@ -2,15 +2,15 @@
     if (document.body.classList.contains('Registration-page') === false) {
         return;
     }
-    const loginFormEL = document.getElementById('RegistrationForm');
+    const registrationFormEL = document.getElementById('RegistrationForm');
     const nameEl = document.getElementById('name');
     const emailEl = document.getElementById('email');
     const passwordEl = document.getElementById('password');
-    loginFormEL.addEventListener('submit', function (e) {
+    registrationFormEL.addEventListener('submit', function (e) {
         e.preventDefault();
         const email = emailEl.value;
         const password = passwordEl.value;
-        requests.login(email, password)
+        requests.register(email, password)
             .catch(() => {
                 alert("Failed")
             })
--- request.js
+++ request.js
@@ -1,12 +1,11 @@
 const axios = require('axios')
 
-module.exports.login = (email, password) => {
+module.exports.register = (email, password) => {
     return new Promise((resolve, reject) => {
         axios.post('http://localhost:5000/register/generate', { email, password })
             .then((res) => {
                 const { refreshToken } = res.data
                 localStorage.setItem('refreshToken', refreshToken)
-                window.location.href = '/dashboard.html'
                 resolve(res.data);
             })
             .catch((err) => {
