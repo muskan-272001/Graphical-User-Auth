027f164ec1d4f1f0c10c36cd08e95f377b9e9ff8
amisha2616 <74150077+amisha2616@users.noreply.github.com>
2022-04-24 21:15:19 +0530

    chore:registration

--- js/registration.js
+++ js/registration.js
@@ -1,18 +1,19 @@
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 const registrationFormEL = document.getElementById('RegistrationForm');
-    const nameEl = document.getElementById('name');
-    const emailEl = document.getElementById('email');
-    registrationFormEL.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const email = emailEl.value;
-        const name = nameEl.value;
-        axios.post('http://localhost:5000/register/generate', { "fullname": name, "email": email })
-            .then((res) => {
-                console.log(res)
-                // const { refreshToken } = res.data
-                // localStorage.setItem('refreshToken', refreshToken)
-                // resolve(res.data);
-            })
-            .catch((err) => {
-                // handleError(err, reject)
-            })
-    });
+const nameEl = document.getElementById('name');
+const emailEl = document.getElementById('email');
+registrationFormEL.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const email = emailEl.value;
+    const name = nameEl.value;
+    axios.post('http://localhost:5000/register/generate', { "fullname": name, "email": email })
+        .then((res) => {
+            console.log(res)
+            // const { refreshToken } = res.data
+            // localStorage.setItem('refreshToken', refreshToken)
+            // resolve(res.data);
+        })
+        .catch((err) => {
+            // handleError(err, reject)
+        })
+});
